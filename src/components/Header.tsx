35f12274a4222000b2ed4dee62f8afec13662567
Devon <devonev92@gmail.com>
2019-09-13 09:51:39 -0700

    added github buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,9 +14,20 @@ const Header: React.FC<HeaderProps> = (props) => {
       <motion.h1 initial="hidden" animate="visible" transition={{ ease: 'easeOut', duration: 2 }} variants={variants}>HOOKD</motion.h1>
       <motion.p animate={{ x: 10 }} transition={{ ease: 'easeOut', duration: 1 }}> A Traspilation and Trasformation tool for React Class Components to Functional Components with Hooks. </motion.p>
       <motion.p animate={{ x: 10 }} transition={{ ease: 'easeOut', duration: 1 }}> Download our
-        <a href="https://www.npmjs.com/package/@reactionaries/hookd"> CLI tool</a>, or find us through
-        <a href="https://github.com/oslabs-beta/Hookd"> Github</a>.
+        <a href="https://www.npmjs.com/package/@reactionaries/hookd"> CLI tool</a>
       </motion.p>
+      <motion.div className = 'github-button-div flex'>
+        <motion.div>
+        <p>Hookd-Client:</p>
+        <a className="github-button" href="https://github.com/oslabs-beta/hookd-client" data-icon="octicon-star" data-show-count="true" aria-label="Star oslabs-beta/hookd-client on GitHub">Star</a>
+        </motion.div>
+        <motion.div>
+        <p>Hookd-CLI:</p>
+        <a className="github-button" href="https://github.com/oslabs-beta/hookd" data-icon="octicon-star" data-show-count="true" aria-label="Star oslabs-beta/hookd on GitHub">Star</a>
+        </motion.div>
+        
+      </motion.div>
+      
       <motion.button 
         id ="submitButton" 
         onClick={props.handleClick}
